e70a2182eada9ae7025b61153887d09b7e7e57e2
yuii88 <pim8828@gmail.com>
2021-10-11 23:53:13 +0700

    create profile image

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Allproduct # ดึง ไฟล์ 
+from .models import Allproduct ,Profile # ดึง ไฟล์ 
 # Register your models here.
 
 admin.site.register(Allproduct) #แสดง Allproduct ที่ Site administration
+admin.site.register(Profile)
--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 # เกี่ยวกับ database การเก็บข้อมูล
+
+class Profile(models.Model):
+	user = models.OneToOneField(User,on_delete=models.CASCADE)
+	photo = models.ImageField(upload_to="gallery_photo_profile",null=True,blank=True)
+	usertype = models.CharField(max_length=100,default='member')
+
+	def __str__(self):
+		return self.user.first_name
+
+
 class Allproduct(models.Model):
 	name = models.CharField(max_length=100)
 	price = models.CharField(max_length=100)
