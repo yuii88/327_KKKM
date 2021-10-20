508a2833980504ef5f292518e3b9a517154ecba7
yuii88 <pim8828@gmail.com>
2021-10-20 23:32:18 +0700

    set admin show all product

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from .models import * # ดึง ไฟล์ 
-# Register your models here.
 
-admin.site.register(Allproduct) #แสดง Allproduct ที่ Site administration
+
+admin.site.site_header = 'KKKM SHOP ADMIN'
+admin.site.index_title = 'Main Admin'
+admin.site.site_title = 'KKKM BACKEND'
+
+class AllproductAdmin(admin.ModelAdmin):
+	list_display = ['name','id','instock','price','quantity']
+	list_editable = ['instock','price','quantity']
+
+admin.site.register(Allproduct,AllproductAdmin) #แสดง Allproduct ที่ Site administration
 admin.site.register(Profile)
 admin.site.register(Cart)
-admin.site.register(OrderList)
+
+
+
+class OrderListAdimin(admin.ModelAdmin):
+	list_display = ['orderid','productname','total']
+
+admin.site.register(OrderList,OrderListAdimin)
 admin.site.register(OrderPending)
